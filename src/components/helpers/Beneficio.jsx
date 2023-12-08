8ac933ede6fcd2289f565f70296784435a9c9a67
CarlosMaeda <carlos.22.10.m@gmail.com>
2023-12-08 16:57:43 -0300

    Corrige estilos

--- src/components/helpers/Beneficio.jsx
+++ src/components/helpers/Beneficio.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 function Beneficio(props) {
   return (
     <>
-      <Card style={{ width: "38rem" }} className="m-4">
-        <Card.Body className=" bg-warning bg-gradient">
+      <Card style={{ width: "38rem", height: "auto" }} className="m-4">
+        <Card.Body className="bg-warning bg-gradient">
           <Card.Title className="fs-3">Beneficio</Card.Title>
           <Card.Text>{props.texto}</Card.Text>
         </Card.Body>
